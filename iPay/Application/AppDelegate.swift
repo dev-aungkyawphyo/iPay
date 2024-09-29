b1c624c9e6b6913bf7fc92033ad6649345ea5214
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-09-30 01:05:37 +0630

    initial commit

---
+++ iPay/Application/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  iPay
+//
+//  Created by Aung Kyaw Phyo on 30/09/2024.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
